04a5d4d5b3185dc9b8d8340632515eb6dfc14a8b
Alex Millane <alex.millane@gmail.com>
2017-07-26 12:30:00 +0200

    Added service to save the grid map.

--- src/library/mesh_to_grid_map_converter.cpp
+++ src/library/mesh_to_grid_map_converter.cpp
@@ -16,7 +16,7 @@ MeshToGridMapConverter::MeshToGridMapConverter(ros::NodeHandle nh,
       grid_map_resolution_(kDefaultGridMapResolution),
       layer_name_(kDefaultLayerName),
       latch_grid_map_pub_(kDefaultLatchGridMapPub),
-      save_to_rosbag_(kDefaultSaveToRosBag),
+      save_to_rosbag_on_publish_(kDefaultSaveToRosBagOnPublish),
       rosbag_topic_name_(kDefaultRosbagTopicName),
       verbose_(kDefaultVerbose) {
   // Initial interaction with ROS
@@ -33,6 +33,8 @@ void MeshToGridMapConverter::subscribeToTopics() {
 void MeshToGridMapConverter::advertiseTopics() {
   grid_map_pub_ = nh_private_.advertise<grid_map_msgs::GridMap>(
       "grid_map", 1, latch_grid_map_pub_);
+  save_grid_map_srv_ = nh_private_.advertiseService(
+      "save_grid_map", &MeshToGridMapConverter::saveGridMapCallback, this);
 }
 
 void MeshToGridMapConverter::getParametersFromRos() {
@@ -41,8 +43,8 @@ void MeshToGridMapConverter::getParametersFromRos() {
   nh_private_.param("layer_name", layer_name_, layer_name_);
   nh_private_.param("latch_grid_map_pub", latch_grid_map_pub_,
                     latch_grid_map_pub_);
-  nh_private_.param("save_to_rosbag", save_to_rosbag_,
-                    save_to_rosbag_);
+  nh_private_.param("save_to_rosbag_on_publish", save_to_rosbag_on_publish_,
+                    save_to_rosbag_on_publish_);
   nh_private_.param("rosbag_filepath", rosbag_file_path_, rosbag_file_path_);
   nh_private_.param("rosbag_topic_name", rosbag_topic_name_,
                     rosbag_topic_name_);
@@ -90,20 +92,41 @@ void MeshToGridMapConverter::meshCallback(
   }
 
   // Saving the gridmap to a rosbag if requested
-  if (save_to_rosbag_) {
-    if (!rosbag_file_path_.empty()) {
-      grid_map::GridMapRosConverter grid_map_ros_converter;
-      if (verbose_) {
-        ROS_INFO_STREAM(
-            "Saved the grid map message to file: " << rosbag_file_path_);
-      }
-      grid_map_ros_converter.saveToBag(map, rosbag_file_path_,
-                                       rosbag_topic_name_);
-    } else {
-      ROS_ERROR(
-          "No rosbag filepath specified (as ros param \"rosbag_file_path\"");
-    }
+  if (save_to_rosbag_on_publish_) {
+    saveGridmap(map);
   }
+
+  // Saving the gridmap to the object
+  last_grid_map_ptr_.reset(new grid_map::GridMap(map));
 }
+
+bool MeshToGridMapConverter::saveGridMapCallback(
+    std_srvs::Empty::Request& request, std_srvs::Empty::Response& response) {
+  // Check there's actually a grid map saved
+  if (!last_grid_map_ptr_) {
+    ROS_ERROR("No grid map produced yet to save.");
+    return false;
+  }
+  return saveGridmap(*last_grid_map_ptr_);
+}
+
+bool MeshToGridMapConverter::saveGridmap(const grid_map::GridMap& map) {
+  // Saving the map
+  if (!rosbag_file_path_.empty()) {
+    grid_map::GridMapRosConverter grid_map_ros_converter;
+    if (verbose_) {
+      ROS_INFO_STREAM(
+          "Saved the grid map message to file: " << rosbag_file_path_);
+    }
+    grid_map_ros_converter.saveToBag(map, rosbag_file_path_,
+                                     rosbag_topic_name_);
+  } else {
+    ROS_ERROR(
+        "No rosbag filepath specified (as ros param \"rosbag_file_path\"");
+    return false;
+  }
+  return true;
+}
+
 
 }  // namespace mesh_to_grid_map
